46b7c32423984a244d21b6167253aae54b3bd1d1
Jesper Nytun <160156297+jespernytun@users.noreply.github.com>
2025-07-05 18:24:19 +0200

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,21 @@
+from RISKsimulator import Kast
+import matplotlib.pyplot as pyplot
+
+N = 1000
+Win = 0
+Loss = 0
+Total = 0
+
+while Total <= N:
+    result = Kast(100,100)
+    if result == True:
+        Win += 1
+        Total += 1
+    elif result == False:
+        Loss += 1
+        Total += 1
+
+xplot = ["wins","losses"]
+yplot = [Win, Loss]
+
+print(yplot)
